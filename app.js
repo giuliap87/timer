9be730163aefe9e765941ac8c069d216dae2ceec
seongeunkimmy <kimusique7@gmail.com>
2021-04-23 18:10:15 +0200

    kim commit

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-alert("hello")
+const hourInput = document.querySelector('#hour');
+const minInput = document.querySelector('#minutes');
+const secInput = document.querySelector('#seconds'); 
+const form = document.querySelector('#setup-form');
+const startBtn = document.querySelector('#start');
+const resetBtn = document.querySelector('#reset'); 
+const display = document.querySelector('#display');
+
+let interval; 
+
+const hours = {
+    input: document.querySelector("#hours"),
+    value: "",
+  };
